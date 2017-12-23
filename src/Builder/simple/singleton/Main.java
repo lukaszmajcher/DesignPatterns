c8e984f627f7ae5b181ab405a8761e265b9b6013
LM1 <majcher.luk@gmail.com>
2017-12-23 08:53:28 +0100

    implementing Singleton pattern

---
+++ src/Builder/simple/singleton/Main.java
@@ -0,0 +1,10 @@
+package Builder.simple.singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton obj1 = Singleton.getInstance();
+        obj1.printObj();
+        Singleton obj2 = Singleton.getInstance();
+        obj2.printObj();
+    }
+}
